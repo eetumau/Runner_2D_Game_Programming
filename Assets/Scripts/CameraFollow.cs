a6a979abdd6d080f318e59f5927db34c2548437a
Eetu Maunuksela <eetu.maunuksela@cs.tamk.fi>
2016-09-09 11:19:18 +0300

    Initial commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Runner
+{
+
+
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform _player;
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = new Vector3(_player.transform.position.x + 6, 0, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Runner
+{
+
+
+    public class InputController : MonoBehaviour
+    {
+
+        private MovementScript _movementController;
+        private bool _jump;
+
+        private void Awake()
+        {
+            _movementController = GetComponent<MovementScript>();
+        }
+
+        private void Update()
+        {
+            if (!_jump)
+                _jump = Input.GetButtonDown("Jump");
+        }
+
+        private void FixedUpdate()
+        {
+            _movementController.Move(1, _jump);
+            _jump = false;
+        }
+    }
+}
